7cd3500d3e1f90929c0fb8f33503175fbd9a7449
codering <zcodeworld@gmail.com>
2017-09-08 17:01:39 +0800

    fix: remove zip plugin

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -371,12 +371,6 @@ module.exports = {
     // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
     // You can remove this if you don't use Moment.js:
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    // pack build use zip
-    new WebpackZipPlugin({
-			initialFile: paths.appBuild,
-			endPath: './',
-			zipName: process.env.ZIP_NAME
-		}),
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
